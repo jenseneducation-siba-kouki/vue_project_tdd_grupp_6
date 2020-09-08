dfe7ca1272b293d0546a915abc9a0cb40dbb5ae7
EllaGotbringg <54885982+EllaGotbringg@users.noreply.github.com>
2020-09-08 12:48:09 +0200

    new products
    products that you can add to cart, counter not working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="home">
-    <Nav />
+    <header>
+      <Nav />
+      {{cart.length}} in cart
+      <button>view cart</button>
+    </header>
+
     <h1>Accessories Web Shop</h1>
+
     <img class="accessories" alt="accessories" src="../assets/photo.jpg" />
 
-    <CountOrder />
+    <h1>products</h1>
+    <div class="wrapper">
+      <div v-for="product in products" :key="product.name">
+        <h2>{{product.name}}</h2>
+
+        <img :src="product.image" width="200" height="200" />
+        <div>{{product.cost}}</div>
+        <button v-on:click="addItemToCart(product)">add to cart</button>
+        <CountOrder />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,6 +36,31 @@ export default {
     Nav,
     CountOrder,
   },
+  data: () => {
+    return {
+      page: "cart",
+      cart: [],
+      products: [
+        {
+          name: "books",
+          cost: "$20",
+          image: "https://hmp.me/dbvr",
+        },
+        {
+          name: "J.R.R Tolkien",
+          cost: "$50",
+          image: "https://hmp.me/dbvs",
+        },
+      ],
+    };
+  },
+  methods: {
+    addItemToCart(product) {
+      console.log(product);
+      this.cart.push(product);
+      console.log(this.cart);
+    },
+  },
 };
 </script>
 <style scoped>
@@ -32,5 +73,13 @@ h1 {
   color: rgb(221, 170, 178);
   font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
     "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  border: 1px black solid;
+}
+header {
+  height: 100px;
 }
 </style>
--- src/views/Productpage.vue
+++ src/views/Productpage.vue
@@ -25,7 +25,7 @@ export default {
   name: "Productspage",
   components: {
     Nav,
-    CountOrder
+    CountOrder,
   },
   data() {
     return {
